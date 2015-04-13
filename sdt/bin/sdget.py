7a4ab0fa531091aed977ef02a141bc213c40f356
jerome <jerome@TOSH001.home>
2015-04-13 08:56:45 +0200

    * Add download method.

--- sdt/bin/sdget.py
+++ sdt/bin/sdget.py
@@ -17,12 +17,13 @@ Note
         - 'wget' based implementation
 """
 
+import os
 import sdapp
 import sdconfig
 from sdtools import print_stderr
+import sdutils
 
 def download_with_wget(url):
-    import os, sdutils
 
     tmpfile='/tmp/sdt_test_file.nc'
 
@@ -39,5 +40,22 @@ def download_with_wget(url):
     if sdget_status==0:
         print 'file location: %s'%tmpfile
     """
+
+def download(url,full_local_path,checksum_type):
+
+    download_cmd_line="%s -c %s %s %s" % (sdconfig.data_download_script,checksum_type,url,full_local_path)
+
+    # start a new process (fork is blocking here, so thread will wait until wget is done)
+    (status,stdout,stderr)=sdutils.get_status_output(download_cmd_line,shell=True)
+
+    if status==0:
+        local_checksum=stdout.rstrip(os.linesep) # if success (status==0), stdout contains only checksum
+    else:
+        local_checksum=None
+
+    # debug (unexpected errors may be hidden in stdout)
+    #print stdout
+
+    return (status,local_checksum,stderr) # if error occurs in 'sdget.sh', stderr contains error message
 
 # init.
