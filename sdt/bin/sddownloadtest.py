9359763325088a5136c4ff988616ff8ffb84a6d8
jerome <jerome@TOSH001.home>
2015-04-26 08:56:09 +0200

    * Add download test script.

---
+++ sdt/bin/sddownloadtest.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+##################################
+#  @program        synchro-data
+#  @description    climate models data transfer program
+#  @copyright      Copyright “(c)2009 Centre National de la Recherche Scientifique CNRS. 
+#                             All Rights Reserved”
+#  @license        CeCILL (http://dods.ipsl.jussieu.fr/jripsl/synchro_data/LICENSE)
+##################################
+
+"""This script run download test on each url and display download status.
+
+Note
+    Return code is always 0
+"""
+
+import argparse
+import os
+import sys
+import json
+import sdapp
+import sdget
+from sdtools import print_stderr
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    files=json.load( sys.stdin )
+
+    for f in files:
+
+        assert 'url' in f
+        assert 'data_node' in f
+
+        url=f['url']
+        dn=f['data_node']
+
+        local_path='/tmp/test.nc'
+
+        if os.path.isfile(local_path):
+            os.remove(local_path)
+
+        (status,local_checksum,killed,script_stdxxx)=sdget.download(url,local_path,checksum_type='md5',debug_level=0)
+
+        if status!=0:
+            print_stderr('Download failed: %s'%dn)
+        else:
+            print_stderr('File successfully downloaded: %s'%dn)
+
+    sys.exit(0)
