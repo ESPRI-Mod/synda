281ef45e4d1e88d9886d959c7244528e502906ed
jerome <jerome@TOSH001.home>
2015-05-03 15:20:45 +0200

    * Redirect interactive msg to stderr.

--- sdt/bin/sdtiaction.py
+++ sdt/bin/sdtiaction.py
@@ -99,12 +99,11 @@ def install(files,args,interactive=True):
 
         return 0
 
-
     # ask user for confirmation
     if interactive:
         import humanize
-        print '%i file(s) will be added to the download queue.'%count_new
-        print 'Once downloaded, %s of additional disk space will be used.'%humanize.naturalsize(size_new,gnu=False)
+        print_stderr('%i file(s) will be added to the download queue.'%count_new)
+        print_stderr('Once downloaded, %s of additional disk space will be used.'%humanize.naturalsize(size_new,gnu=False))
 
         import sdutils
         if sdutils.query_yes_no('Do you want to continue?', default="yes"):
@@ -121,11 +120,11 @@ def install(files,args,interactive=True):
         sdenqueue.run(files)
 
         if interactive:
-            print "%i file(s) enqueued"%count_new
-            print 'You can start the daemon to begin the download.' # TODO
+            print_stderr("%i file(s) enqueued"%count_new)
+            print_stderr('You can start the daemon to begin the download.') # TODO
     else:
         if interactive:
-            print 'Abort.'
+            print_stderr('Abort.')
 
     return count_new
 
@@ -143,14 +142,14 @@ def remove(files,args):
 
         if count_delete>0:
 
-            print '%i file(s) will be removed.'%count_delete
-            print 'After this operation, %s of disk space will be freed.'%humanize.naturalsize(size_delete,gnu=False)
+            print_stderr('%i file(s) will be removed.'%count_delete)
+            print_stderr('After this operation, %s of disk space will be freed.'%humanize.naturalsize(size_delete,gnu=False))
 
             if sdutils.query_yes_no('Do you want to continue?', default="no"):
 
                 # first step, change the files status from 'done' to 'delete' (update metadata)
                 nbr=sddelete.run(files)
-                print "%i file(s) removed"%nbr
+                print_stderr("%i file(s) removed"%nbr)
 
                 # second step, do the deletion (remove files on filesystem and remove files metadata)
                 # (to do a deferred deletion (i.e. by the daemon), comment line below)
@@ -163,9 +162,9 @@ def remove(files,args):
                 sdoperation.cleanup_tree()
 
             else:
-                print 'Abort.'
+                print_stderr('Abort.')
         else:
-            print 'Nothing to delete.'
+            print('Nothing to delete.')
 
 def reset(args):
     import sddeletefile
@@ -206,7 +205,7 @@ def upgrade(args):
     project=sdparameter.extract_values_from_parameter(args.parameter,'project') # retrieve project(s) from parameter
 
     for selection in sdselectionsgroup.get_selection_list(project=project):
-        print "Process %s.."%selection.filename
+        print_stderr("Process %s.."%selection.filename)
 
         if not args.dry_run:
 
@@ -256,9 +255,9 @@ def retry(args):
     import sdmodify
     nbr=sdmodify.retry_all()
     if nbr>0:
-        print "%i file(s) marked for retry."%nbr
+        print_stderr("%i file(s) marked for retry."%nbr)
     else:
-        print "No transfer in error"
+        print_stderr("No transfer in error")
 
 def daemon(args):
     import sddaemon
@@ -269,7 +268,7 @@ def daemon(args):
         action=args.parameter[0] # it's a naming mess: rename top level action as subcommand
         if action=="start":
             if sddaemon.is_running():
-                print "Daemon already started"
+                print_stderr("Daemon already started")
             else:
                 try:
                     sddaemon.start()
