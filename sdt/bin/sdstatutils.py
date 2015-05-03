9f0907f39a5f0a650f5546a098006065291801d4
jerome <jerome@TOSH001.home>
2015-05-03 11:47:05 +0200

    * Replace LICENSE link (from dods to github).

--- sdt/bin/sdstatutils.py
+++ sdt/bin/sdstatutils.py
@@ -6,7 +6,7 @@
 #  @description    climate models data transfer program
 #  @copyright      Copyright “(c)2009 Centre National de la Recherche Scientifique CNRS. 
 #                             All Rights Reserved”
-#  @license        CeCILL (http://dods.ipsl.jussieu.fr/jripsl/synchro_data/LICENSE)
+#  @license        CeCILL (https://raw.githubusercontent.com/Prodiguer/synda/master/sdt/doc/LICENSE)
 ##################################
 
 """This module contains stat generic routines."""
