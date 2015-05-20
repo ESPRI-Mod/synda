4b1a64338975e7073e493b16106d607b9a1a1be0
jerome <jerome@TOSH001.home>
2015-05-20 11:33:54 +0200

    * Add 'help' action.

--- sdt/bin/synda.py
+++ sdt/bin/synda.py
@@ -62,6 +62,8 @@ if __name__ == '__main__':
     if args.action in ['autoremove','cache','certificate','daemon','history','queue','replica','reset','retry','selection','test','upgrade','watch']:
         import sdtiaction
         sdtiaction.actions[args.action](args)
+    elif args.action=='help':
+        parser.print_help()
     elif args.action=='update':
         print_stderr('Not implemented yet.')   
     elif args.action=='param':
