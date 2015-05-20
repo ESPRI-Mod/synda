da11cdce984a1b252184955b2047253670358846
jerome <jerome@TOSH001.home>
2015-05-20 11:08:23 +0200

    * Fix indent.

--- sdt/setup.py
+++ sdt/setup.py
@@ -2,11 +2,12 @@
 from distutils.core import setup
 import glob
 import os
+
 setup(name='sdt',
-		#py_modules=['sdutils'],
-		scripts=[os.path.join('bin',f) for f in os.listdir('bin')],
-		data_files=[('conf',['conf/sdt.conf']),
-		            ('conf/default',glob.glob(os.path.join('conf/default', '*.txt'))),
+        #py_modules=['sdutils'],
+        scripts=[os.path.join('bin',f) for f in os.listdir('bin')],
+        data_files=[('conf',['conf/sdt.conf']),
+                    ('conf/default',glob.glob(os.path.join('conf/default', '*.txt'))),
                     ('selection', glob.glob(os.path.join('selection', '*.txt'))),
                     ('selection/sample', glob.glob(os.path.join('selection/sample', '*'))),
                     ('doc',['doc/LICENSE']),
@@ -15,12 +16,12 @@ setup(name='sdt',
                     ('log',''),
                     ('tmp',''),
                     ('db','')], 
-		url='https://github.com/Prodiguer/synda',
-		version='3.0',
-		description='ESGF Data transfer Program',
-		long_description='This program download files from the Earth System Grid Federation (ESGF) archive using command line.',
-		license='Public',
-		platforms='Linux',
-		author='jripsl',
-		author_email='jripsl@ipsl.jussieu.fr'
-		)
+        url='https://github.com/Prodiguer/synda',
+        version='3.0',
+        description='ESGF Data transfer Program',
+        long_description='This program download files from the Earth System Grid Federation (ESGF) archive using command line.',
+        license='Public',
+        platforms='Linux',
+        author='jripsl',
+        author_email='jripsl@ipsl.jussieu.fr'
+        )
