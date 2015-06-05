6a595e2d904baea2bc6e67849a9040d31cc5c2d1
jerome <jerome@TOSH001.home>
2015-06-05 13:41:36 +0200

    * Include end bound in range() func.

--- sdt/bin/sddbversionutils.py
+++ sdt/bin/sddbversionutils.py
@@ -23,13 +23,34 @@ def _int2str(version):
     assert isinstance(version,int)
     return str(float(version)/10)
 
+def inclusive_range(vb,ve):
+    """Same as range(), except that end bound is included."""
+
+    assert isinstance(vb,int)
+    assert isinstance(ve,int)
+
+    if ve > vb:
+
+        # we need to transform 've', because range() func do not return last value
+        #
+        ve+=1 # e.g. if last was 33, it is now 34 and 34 will be stripped by range()
+
+        return range(vb,ve)
+
+    else:
+        # in this case, return empty list as no upgrade needed
+
+        return []
+
 def version_range(version_begin,version_end):
     """Return version range between version_begin and version_end."""
 
     vb=_str2int(version_begin)
     ve=_str2int(version_end)
 
-    return [_int2str(i) for i in range(vb,ve)]
+    li=[_int2str(i) for i in inclusive_range(vb,ve)]
+
+    return li
 
 # --- version table I/O --- #
 
