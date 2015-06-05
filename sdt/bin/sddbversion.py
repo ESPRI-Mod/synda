247ca05faf9e35b28c48346abc4d4264ceae894f
jerome <jerome@TOSH001.home>
2015-06-05 14:14:13 +0200

    * Add database schema automatic upgrade.

---
+++ sdt/bin/sddbversion.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+##################################
+#  @program        syndat
+#  @description    climate models data transfer program
+#  @copyright      Copyright “(c)2009 Centre National de la Recherche Scientifique CNRS. 
+#                             All Rights Reserved”
+#  @license        CeCILL (https://raw.githubusercontent.com/Prodiguer/synda/master/sdt/doc/LICENSE)
+##################################
+
+"""This module contains database versioning routines."""
+
+import sdapp
+import sddbversionutils
+from sdexception import SDException
+import sdlog
+
+def check_version(conn):
+    """Upgrade the database schema if database version does not match binary version."""
+
+    current_db_version=sddbversionutils.get_db_version(conn)
+
+    if current_db_version==None:
+        # this case is for when starting from scratch
+        # (first startup or when db file as been removed)
+
+        c = conn.cursor()
+        c.execute("insert into version (version) values (?)",(sdapp.version,))
+        conn.commit()
+        c.close()
+    else:
+        if current_db_version < "2.9":
+            raise SDException("SDDBVERS-316","Database version too old: cannot upgrade database")
+
+        if sdapp.version==current_db_version:
+            pass # db version matches binary version, nothing to do
+        elif sdapp.version < current_db_version:
+            raise SDException("SDDBVERS-317","Binary cannot be used with this database (binary version too old)")
+        elif sdapp.version > current_db_version:
+            upgrade_db(conn,current_db_version,sdapp.version)
+
+def upgrade_db(conn,current_db_version,new_db_version):
+    li=sddbversionutils.version_range(current_db_version,new_db_version)
+
+    # remove the first value (i.e. no upgrade needed there as db is already at this version)
+    li=li[1:]
+
+    for v in li:
+        if v not in upgrade_procs:
+            raise SDException("SDDBVERS-318","Incorrect database version: cannot upgrade database (version=%s)."%(v,))
+        else:
+            upgrade_procs[v](conn)
+            sdlog.info("SDDBVERS-319","Database updated to version %s"%(v,))
+
+# -- upgrade procs -- #
+
+def upgrade_31(conn):
+
+    # put schema upgrade code if any
+
+    sddbversionutils.update_db_version(conn,'3.1')
+
+def upgrade_30(conn):
+
+    # put schema upgrade code if any
+
+    sddbversionutils.update_db_version(conn,'3.0')
+
+# init.
+
+upgrade_procs={
+    '3.1': upgrade_31,
+    '3.0': upgrade_30
+}
