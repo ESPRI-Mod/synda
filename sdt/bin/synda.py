c037183d4f305826264a4bcbfeb3c6f9d5baa832
jerome <jerome@TOSH001.home>
2015-07-23 22:47:16 +0200

    * Add argparse subparsers. * synda.py refactoring.

--- sdt/bin/synda.py
+++ sdt/bin/synda.py
@@ -11,9 +11,11 @@
 
 """'synda' command (front-end to Synda main commands).
 
-Note
-    - In this file, most module import directives are moved near the calls,
+Notes
+    - in this file, most module import directives are moved near the calls,
       so to improve startup time (even for sdapp).
+    - do not import 'sdlog' at the beggining of this file, because in this case, it breaks the daemon startup (i.e. double-fork problem) !
+    - do not put a dry_run test here (sdtiaction's funcs are called from other place too, so the dry_run test need to be done inside sdtiaction's funcs)
 """
 
 import sys
@@ -29,6 +31,37 @@ def exec_action(args):
     method = getattr(sdaction, args.subparser_name)
     result = method()
 """
+
+def check_daemon():
+    import sdconfig
+    if sdconfig.prevent_daemon_and_modification:
+        import sddaemon
+        if sddaemon.is_running():
+            print 'The daemon must be stopped before installing/removing dataset'
+            sys.exit(3)
+
+def get_stream(args):
+        import sdbuffer, sdparse, sdstream, sdconfig, sddeferredbefore
+
+        # hack
+        if args.action=='list':
+            args.no_default=True
+
+        buffer=sdbuffer.get_selection_file_buffer(parameter=args.parameter,path=args.selection)
+        selection=sdparse.build(buffer,load_default=(not args.no_default))
+        stream=selection.to_stream()
+
+        # Set default value for nearest here
+        #
+        # TODO: make it work with all actions (e.g. search) as it only working for 'install' action for now
+        #
+        #sddeferredbefore.add_default_parameter(stream,'nearest',True) # TODO: why this one is not working ?
+        if sdconfig.config.getboolean('behaviour','nearest'):
+            sdstream.set_scalar(stream,'nearest',True)
+
+        # progress
+        if sdconfig.config.getboolean('interface','progress'):
+            sdstream.set_scalar(stream,'progress',True)
 
 if __name__ == '__main__':
     """
@@ -110,116 +143,81 @@ if __name__ == '__main__':
     elif args.action=='param':
         import sdparam
         sdparam.main(args.parameter) # tricks to re-use sdparam CLI parser
-    elif args.action in ['dump','install','list','remove','search','show','stat','version']:
-        import sdbuffer, sdparse, sdstream, sdconfig, sddeferredbefore
 
-        # hack
-        if args.action=='list':
-            args.no_default=True
+    elif args.action in ['dump','list','search','show','version']:
+        import sddeferredbefore
 
-        buffer=sdbuffer.get_selection_file_buffer(parameter=args.parameter,path=args.selection)
-        selection=sdparse.build(buffer,load_default=(not args.no_default))
-        stream=selection.to_stream()
+        stream=get_stream(args)
 
-        # Set default value for nearest here
+        # Set default type
+        if args.type_ is None:
+            import sdtype
+            args.type_=sdtype.infer_display_type(stream)
+
+        # Set the sdtream type (aka search-API 'type').
         #
-        # TODO: make it work with all actions (e.g. search) as it only working for 'install' action for now
+        # Note that arg.type_ is NOT the same thing as the stream type (aka
+        # search-API type). arg.type_ is only used locally to format the
+        # listing presented to user, while the stream type is the one sent
+        # to the ESGF service to retrieve data. For example,
+        # SA_TYPE_AGGREGATION is used by arg.type_ to make some change in
+        # the output, but search-API don't know about this type (i.e. for
+        # most project, you can't list anything by using this type). Also
+        # most modules of Synda behave the same way as search-API: they
+        # don't know about SA_TYPE_AGGREGATION. SA_TYPE_AGGREGATION is ONLY
+        # used in Synda upstream code to make some local display
+        # modifications.
         #
-        #sddeferredbefore.add_default_parameter(stream,'nearest',True) # TODO: why this one is not working ?
-        if sdconfig.config.getboolean('behaviour','nearest'):
-            sdstream.set_scalar(stream,'nearest',True)
-
-        # progress
-        if sdconfig.config.getboolean('interface','progress'):
-            sdstream.set_scalar(stream,'progress',True)
-
-        if args.action in ['dump','list','search','show','version']:
-
-            # Set default type
-            if args.type_ is None:
-                import sdtype
-                args.type_=sdtype.infer_display_type(stream)
-
-            # Set the sdtream type (aka search-API 'type').
-            #
-            # Note that arg.type_ is NOT the same thing as the stream type (aka
-            # search-API type). arg.type_ is only used locally to format the
-            # listing presented to user, while the stream type is the one sent
-            # to the ESGF service to retrieve data. For example,
-            # SA_TYPE_AGGREGATION is used by arg.type_ to make some change in
-            # the output, but search-API don't know about this type (i.e. for
-            # most project, you can't list anything by using this type). Also
-            # most modules of Synda behave the same way as search-API: they
-            # don't know about SA_TYPE_AGGREGATION. SA_TYPE_AGGREGATION is ONLY
-            # used in Synda upstream code to make some local display
-            # modifications.
-            #
-            # So what we do here is choose which is the search-API type we need
-            # (dataset, file) for the listing type asked by user (i.e.
-            # variable, dataset, file)
-            #
-            # But note that in most case, search-API 'type' will be overrided
-            # later anyway as it is forced in dedicated modules (e.g. in
-            # sdrdataset, sdrfile, etc..).
-            #
-            # Also note that we 'force' (i.e. not 'default') the parameter
-            # here, so to prevent user to set it. We do this because if user
-            # use '-f' option and type=Dataset, it will not do as the display
-            # type will not fit the type of data fetched from search-API).
-            #
-            if args.type_ in (sdconst.SA_TYPE_AGGREGATION,sdconst.SA_TYPE_DATASET):
-                sddeferredbefore.add_forced_parameter(stream,'type',sdconst.SA_TYPE_DATASET)
-            elif args.type_ in (sdconst.SA_TYPE_FILE,):
-                sddeferredbefore.add_forced_parameter(stream,'type',sdconst.SA_TYPE_FILE)
-            else:
-                from sdexception import SDException
-                raise SDException('SDASYNDA-001','Unknown type (%s)'%args.type_)
-
-            args.stream=stream # hack: pass 'stream' object downstream as a standalone argument (not inside args)
-
-            import sdtsaction
-            sdtsaction.actions[args.action](args)
-
-        elif args.action in ['remove','install','stat']:
-            # those actions systematically trigger full search (i.e. limit keyword cannot be used here)
-
-            # check
-            if sdconfig.prevent_daemon_and_modification:
-                if args.action in ['remove','install']:
-                    import sddaemon
-                    if sddaemon.is_running():
-                        print 'The daemon must be stopped before installing/removing dataset'
-                        sys.exit(3)
-
-            if sdstream.is_empty(stream):
-                print 'No packages will be installed, upgraded, or removed.'
-            else:
-
-                # memo tuning: until this point, it go fast
-
-                # Set the sdtream type.
-                # Note that we 'force' (i.e. not 'default') the parameter here, so to prevent user to set it
-                # (i.e. the type is always SA_TYPE_FILE when we are here).
-                sddeferredbefore.add_forced_parameter(stream,'type',sdconst.SA_TYPE_FILE)
-
-                import sdsearch
-                files=sdsearch.run(stream=stream,dry_run=args.dry_run)
-
-                """ DEBUG
-                for f in files:
-                    import sdlog # do NOT import 'sdlog' at the beggining of this file, because in this case, it breaks the daemon startup (i.e. double-fork problem) !
-                    sdlog.debug("SDASYNDA-002","%s"%f['url'],stdout=True)
-                """
-
-                # note: do not put a dry_run test here
-                #       (sdtiaction's funcs are called from other place too, so the dry_run test need to be done inside sdtiaction's funcs)
-
-                import sdtiaction
-                sdtiaction.actions[args.action](files,args)
+        # So what we do here is choose which is the search-API type we need
+        # (dataset, file) for the listing type asked by user (i.e.
+        # variable, dataset, file)
+        #
+        # But note that in most case, search-API 'type' will be overrided
+        # later anyway as it is forced in dedicated modules (e.g. in
+        # sdrdataset, sdrfile, etc..).
+        #
+        # Also note that we 'force' (i.e. not 'default') the parameter
+        # here, so to prevent user to set it. We do this because if user
+        # use '-f' option and type=Dataset, it will not do as the display
+        # type will not fit the type of data fetched from search-API).
+        #
+        if args.type_ in (sdconst.SA_TYPE_AGGREGATION,sdconst.SA_TYPE_DATASET):
+            sddeferredbefore.add_forced_parameter(stream,'type',sdconst.SA_TYPE_DATASET)
+        elif args.type_ in (sdconst.SA_TYPE_FILE,):
+            sddeferredbefore.add_forced_parameter(stream,'type',sdconst.SA_TYPE_FILE)
         else:
-            # we shouldn't be here
+            from sdexception import SDException
+            raise SDException('SDASYNDA-001','Unknown type (%s)'%args.type_)
 
-            assert False
+        args.stream=stream # hack: pass 'stream' object downstream as a standalone argument (not inside args)
+
+        import sdtsaction
+        sdtsaction.actions[args.action](args)
+
+    elif args.action in ['remove','install','stat']:
+        import sdstream, sddeferredbefore
+
+        stream=get_stream(args)
+
+        # those actions systematically trigger full search (i.e. limit keyword cannot be used here)
+
+        if args.action in ['remove','install']:
+            check_daemon()
+
+        if sdstream.is_empty(stream):
+            print 'No packages will be installed, upgraded, or removed.'
+            sys.exit(0)
+
+        # Set the sdtream type.
+        # Note that we 'force' (i.e. not 'default') the parameter here, so to prevent user to set it
+        # (i.e. the type is always SA_TYPE_FILE when we are here).
+        sddeferredbefore.add_forced_parameter(stream,'type',sdconst.SA_TYPE_FILE)
+
+        import sdsearch
+        files=sdsearch.run(stream=stream,dry_run=args.dry_run)
+
+        import sdtiaction
+        sdtiaction.actions[args.action](files,args)
     else:
         print_stderr('Invalid operation %s'%args.action)   
         print_stderr("Use '--help' option for more info")
