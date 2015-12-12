81bd647996f5ec74bea0e7f9ccd714b161da4504
jripsl <jerome915@gmail.com>
2015-12-12 20:43:09 +0100

    * Add sdignorecase module.

---
+++ sdt/bin/sdignorecase.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+##################################
+#  @program        synda
+#  @description    climate models data transfer program
+#  @copyright      Copyright “(c)2009 Centre National de la Recherche Scientifique CNRS. 
+#                             All Rights Reserved”
+#  @license        CeCILL (https://raw.githubusercontent.com/Prodiguer/synda/master/sdt/doc/LICENSE)
+##################################
+
+"""Fix incorrect case for parameter value.
+
+Example
+    cmip5 (incorrect case) is transformed into CMIP5
+"""
+
+import sys
+import argparse
+import json
+import sdapp
+from sdexception import SDException
+import sdconst
+import sdparam
+import sdi18n
+import sddquery
+import sdidtest
+import sdconfig
+import sdprint
+
+def run(facets_groups):
+    for facets_group in facets_groups:
+        type_=sddquery.get_scalar(facets_group,'type',default=sdconst.SD_TYPE_DEFAULT) 
+
+        if sdconst.PENDING_PARAMETER in facets_group:
+            pending_parameters=facets_group[sdconst.PENDING_PARAMETER]
+
+            for pvalue in pending_parameters:
+
+                # HACK: this is to prevent 'SYDPARAM-002' exception when using the following construct 'variable[*]=sic evap' in selection file
+                if pvalue=='*':
+                    continue
+
+                pname=infere_parameter_name(pvalue,type_)
+                facets_group[pname].append(pvalue)
+
+            del facets_group[sdconst.PENDING_PARAMETER]
+
+    return facets_groups
+
+def infere_parameter_name(pvalue,type_):
+    if pvalue.isdigit():
+        pname='limit'
+    elif sdidtest.is_file_functional_id(pvalue):
+        pname='query'
+    elif sdidtest.is_filename(pvalue):
+        pname='title'
+    elif sdidtest.is_dataset_functional_id(pvalue):
+        if type_==sdconst.SA_TYPE_FILE:
+
+            if sdconfig.dataset_filter_mecanism_in_file_context=='query':
+
+                # Use ESGF free text 'query' parameter
+                #
+                # Beware: this mode is experimental and seems not reliable (see TAG543N45K3KJK for info )
+                #
+                pname='query'
+
+            elif sdconfig.dataset_filter_mecanism_in_file_context=='dataset_id':
+
+                # Use 'dataset_id' parameter with the 'sdcompletedatasetid' filter
+                #
+                # Beware: this trigger a search-api call in 'sdcompletedatasetid' filter
+                #
+                pname='dataset_id'
+
+            else:
+                raise SDException('SDINFERE-040','Unknown value (%s)'%sdconfig.dataset_filter_mecanism_in_file_context)
+
+        elif type_==sdconst.SA_TYPE_DATASET:
+            pname='instance_id'
+        else:
+            raise SDException('SDINFERE-001','Unknown type (%s)'%type_)
+    elif sdidtest.is_dataset_local_path(pvalue):
+        pname='local_path'
+    elif sdidtest.is_file_local_path(pvalue):
+        pname='local_path'
+    else:
+        try:
+            pname=sdparam.get_name_from_value(pvalue)
+        except SDException,e:
+            if sdconfig.config.getint('behaviour','check_parameter')==1:
+                raise
+            else:
+                pname='query' # fallback to free pattern
+
+    return pname
+
+# module init.
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-1','--print_only_one_item',action='store_true')
+    parser.add_argument('-f','--format',choices=['raw','line','indent'],default='raw')
+    args = parser.parse_args()
+
+    facets_groups=json.load( sys.stdin )
+    facets_groups=run(facets_groups)
+    sdprint.print_format(facets_groups,args.format,args.print_only_one_item)
