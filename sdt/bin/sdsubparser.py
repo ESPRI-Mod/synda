ae7482d182ca3aaf0994fd2dfc508ff94ef859fa
jripsl <jripsl@outlook.com>
2016-03-08 10:46:40 +0100

    Make type_ options mutually exclusive.
    Replace add_argument_group with add_mutually_exclusive_group.

--- sdt/bin/sdsubparser.py
+++ sdt/bin/sdsubparser.py
@@ -22,7 +22,7 @@ def add_verbose_option(parser):
     parser.add_argument('--verbose',action='store_true',help='verbose mode')
 
 def add_type_grp(parser):
-    type_grp=parser.add_argument_group(None)
+    type_grp=parser.add_mutually_exclusive_group(required=False)
     type_grp.add_argument('-a','--aggregation',dest='type_',action='store_const',const=sdconst.SA_TYPE_AGGREGATION)
     type_grp.add_argument('-d','--dataset',dest='type_',action='store_const',const=sdconst.SA_TYPE_DATASET)
     type_grp.add_argument('-f','--file',dest='type_',action='store_const',const=sdconst.SA_TYPE_FILE)
