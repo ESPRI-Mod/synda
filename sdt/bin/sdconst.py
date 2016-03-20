1add554180e6e12870a8a8b35832f0382ee2a89d
jripsl <jripsl@outlook.com>
2016-03-20 20:22:29 +0100

    Move DEFAULT_SESSION_PARAMS into sdr module.

--- sdt/bin/sdconst.py
+++ sdt/bin/sdconst.py
@@ -141,23 +141,6 @@ SELECTION_FROM_STDIN='stdin'
 #
 PROJECT_WITH_ONE_VARIABLE_PER_DATASET=['CORDEX']
 
-#    name,           type_, default_value,               search_api_facet, option
-# TODO => what is 'option' flag for ??? (i.e. is just the exact opposite of 'search_api_facet' flag, isn't it ?
-DEFAULT_SESSION_PARAMS=[
-    ['verbose',      bool,  True,                         False,           True],
-    ['ignorecase',   bool,  False,                        False,           True],
-    ['onemgf',       bool,  False,                        False,           True],
-    ['last_query',   str,   '',                           False,           True],
-    ['dry_run',      bool,  False,                        False,           True],
-    ['debug',        bool,  False,                        False,           True],
-    ['localsearch',  bool,  False,                        False,           True],
-    ['replica',      bool,  False,                        False,           False],
-    ['limit',        int,  '20',                          False,           False],
-    ['distrib',      bool,  True,                         False,           False],
-    ['latest',       bool,  None,                         False,           False],
-    ['type',         str,   SD_TYPE_DEFAULT,              False,           False]
-]
-
 # Light fields are used to retrieve just the necessary fields, so to limit the bandwidth footprint.
 #
 # Notes
