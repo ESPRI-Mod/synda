18361cc34807881c907f0bb4590352cec8f74bac
jripsl <jripsl@outlook.com>
2016-03-30 05:41:52 +0200

    Fix incorrect argument handling.

--- sdt/bin/sdlogon.py
+++ sdt/bin/sdlogon.py
@@ -83,11 +83,7 @@ def renew_certificate(force_renew_certificate,quiet=True,debug=False,force_renew
 
     (hostname,port,username)=sdopenid.extract_info_from_openid(openid)
 
-    argv=[sdconfig.logon_script]
-    argv.append('-h %s'%hostname)
-    argv.append('-p %s'%port)
-    argv.append('-s %s'%sdconfig.security_dir)
-    argv.append('-u %s'%username)
+    argv=[sdconfig.logon_script,'-h',hostname,'-p',port,'-s',sdconfig.security_dir,'-u',username]
 
     if not quiet:
         argv.append('-v')
