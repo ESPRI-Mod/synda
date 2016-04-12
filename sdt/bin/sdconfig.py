bd67ea0417c15bc3efaff6ed61ff179025c9c44d
jripsl <jripsl@outlook.com>
2016-04-12 10:36:31 +0200

    Fix indent.

--- sdt/bin/sdconfig.py
+++ sdt/bin/sdconfig.py
@@ -20,16 +20,16 @@ from sdexception import SDException
 # this module do not import 'sdlog' as used by sddaemon module (i.e. double fork pb)
 
 def get_data_folder():
-        path=config.get('core','data_path')
-        if len(path)>0: # would be better to test with None (to do that, try using 'allow_no_value' when configuring ConfigParser)
-            return path
+    path=config.get('core','data_path')
+    if len(path)>0: # would be better to test with None (to do that, try using 'allow_no_value' when configuring ConfigParser)
+        return path
 
     return "%s/data"%root_folder
 
 def get_db_folder():
-        path=config.get('core','db_path')
-        if len(path)>0: # would be better to test with None (to do that, try using 'allow_no_value' when configuring ConfigParser)
-            return path
+    path=config.get('core','db_path')
+    if len(path)>0: # would be better to test with None (to do that, try using 'allow_no_value' when configuring ConfigParser)
+        return path
 
     return "%s/db"%root_folder
 
