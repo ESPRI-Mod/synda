22f0a04215e2b5ae2d530c345f7ab6eeea5838ac
jripsl <jripsl@outlook.com>
2016-05-01 10:40:23 +0200

    'synda variable' impl. (suite).

--- sdt/bin/sdtiaction.py
+++ sdt/bin/sdtiaction.py
@@ -505,17 +505,78 @@ def update(args):
     print_stderr("Parameters are up-to-date.")
 
 def variable(args):
-    import sdparam,sdremoteparam,syndautils,sdinference
+    import sdremoteparam,sdproxy,sdutils
 
-    params=sdremoteparam.run(pname='variable,cf_standard_name,variable_long_name',dry_run=args.dry_run)
+    # currently, mode (list or show) is determined by
+    # parameter existency. This may change in the future
+    # as it may be useful to list variable based on filter
+    # (e.g. list variable long name only for obs4MIPs
+    # project, etc..). To do that, we will need to 
+    # add an 'action' argument (i.e. list and show).
+    #
+    action='show' if len(args.parameter)>0 else 'list'
 
-    print params
-    # TODO: func for code below
-    """
-    items=params.get(args.facet_name,[])
-    for item in items:
-        print item.name
-    """
+    if action=='list':
+
+        if args.long_name:
+            facet='variable_long_name'
+        elif args.short_name:
+            facet='variable'
+        elif args.standard_name:
+            facet='cf_standard_name'
+        else:
+            # no options set by user
+
+            facet='variable_long_name' # default
+            
+
+        params=sdremoteparam.run(pname=facet,dry_run=args.dry_run)
+
+        if not args.dry_run:
+
+            # This try/except block is to prevent
+            # IOError: [Errno 32] Broken pipe
+            # Other way to prevent it is to ignore SIGPIPE
+            # More info at
+            # http://stackoverflow.com/questions/14207708/ioerror-errno-32-broken-pipe-python
+            try:
+
+                # TODO: func for code below
+                items=params.get(facet)
+                for item in items:
+                    print item.name
+
+            except:
+                pass
+
+    elif action=='show':
+
+        # We do not use inference here, instead we use
+        # search-api 'query' feature to do the job.
+        #
+        query=sdutils.parameter_to_query(args.parameter)
+        files=sdproxy.call_searchapi_light(query=query,dry_run=args.dry_run)
+
+        if not args.dry_run:
+
+            if len(files)==0:
+
+                print 'Variable not found.'
+
+            elif len(files)==1:
+
+                file_=files[0] # indice 0 is because we retrieve one file only (with 'limit=1')
+
+                if len(file_['variable'])>1:
+                    print "WARNING: 'variable' attribute contains too much values ('%s')."%file_['title']
+                else:
+                    print 'short name:       ',file_['variable'][0]
+                    print 'standard name:    ',file_['cf_standard_name'][0]
+                    print 'long name:        ',file_['variable_long_name'][0]
+                    print 'unit:             ',file_['variable_units'][0]
+
+            else:
+                assert False
 
 def watch(args):
     import sdreport, sddaemon
