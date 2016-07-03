faa1049c555d7306672a6b7cf38bdc70545839b8
jripsl <jripsl@outlook.com>
2016-07-03 20:02:39 +0200

    Add synda_pp command.

---
+++ sdp/bin/synda_pp.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+##################################
+#  @program        synda
+#  @description    climate models data transfer program
+#  @copyright      Copyright “(c)2009 Centre National de la Recherche Scientifique CNRS. 
+#                             All Rights Reserved”
+#  @license        CeCILL (https://raw.githubusercontent.com/Prodiguer/synda/master/sdt/doc/LICENSE)
+##################################
+
+"""'synda_pp' command.
+"""
+
+import sys
+import argparse
+import spapp
+import spsubparser
+import sptools
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    subparsers = parser.add_subparsers(dest='subcommand',metavar='subcommand')
+    parser.add_argument('-V','--version',action='version',version=sdapp.version) # beware: version exist both as option and as subcommand
+    sdsubparser.run(subparsers)
+    args = parser.parse_args()
+
+    # -- subcommand routing -- #
+
+    if args.subcommand=='help':
+
+        if args.topic is None:
+            parser.print_help()
+        else:
+            if args.topic in subparsers.choices:
+                subparsers.choices[args.topic].print_help()
+            else:
+                sptools.print_stderr('Help topic not found (%s)'%args.topic)
+
+        sys.exit(0)
+
+    import sdtsaction
+    if args.subcommand in spaction.actions.keys():
+        sdtsaction.actions[args.subcommand](args)
+        status=sdtiaction.actions[args.subcommand](args)
+
+        # hack
+        # TODO: review all return code in sdtiaction module
+        if not isinstance(status,int):
+            status=0 # arbitrary
+
+        sys.exit(status)
+
+    sptools.print_stderr('Invalid operation %s'%args.subcommand)   
+    sptools.print_stderr("Use '--help' option for more info")
+    sys.exit(2)
