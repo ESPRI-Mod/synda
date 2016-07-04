de8b44933171ea2ed125e9bcef4303557568ac74
jripsl <jripsl@outlook.com>
2016-07-04 11:58:34 +0200

    Do not create credential file if already exist.

--- sdt/bin/sdconfig.py
+++ sdt/bin/sdconfig.py
@@ -230,10 +230,11 @@ if not is_special_user():
     # infos.)
 
     # create user credential file sample
-    if not os.path.exists(user_conf_dir):
-        os.makedirs(user_conf_dir)
-    sdcfbuilder.create_credential_file_sample(user_credential_file)
-    os.chmod(user_credential_file,0600)
+    if not os.path.exists(user_credential_file):
+        if not os.path.exists(user_conf_dir):
+            os.makedirs(user_conf_dir)
+        sdcfbuilder.create_credential_file_sample(user_credential_file)
+        os.chmod(user_credential_file,0600)
 
 config=sdcfloader.load(configuration_file,credential_file,user_configuration_file,user_credential_file,special_user=is_special_user())
 
