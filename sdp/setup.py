4fef7ea7536660acdad624a908f866f006a32341
jripsl <jripsl@outlook.com>
2016-07-04 20:02:33 +0200

    Replace tab with space.

--- sdp/setup.py
+++ sdp/setup.py
@@ -2,22 +2,22 @@ from distutils.core import setup
 import glob
 import os
 setup(name='sdp',
-		#py_modules=['ppdb'],
-		scripts=[os.path.join('bin',f) for f in os.listdir('bin')], 
-		data_files=[('conf',['conf/sdp.conf']),
-		            ('conf/pipeline',glob.glob(os.path.join('conf/pipeline', '*'))),
+        #py_modules=['ppdb'],
+        scripts=[os.path.join('bin',f) for f in os.listdir('bin')], 
+        data_files=[('conf',['conf/sdp.conf']),
+                    ('conf/pipeline',glob.glob(os.path.join('conf/pipeline', '*'))),
                     ('doc',['doc/LICENSE']),
                     ('tools', glob.glob(os.path.join('tools', '*'))),
                     ('data',''),
                     ('log',''),
                     ('tmp',''),
                     ('db','')], 
-		url='https://github.com/Prodiguer/synda',
-		version='1.0',
-		description='ESGF Data Processing Program',
-		long_description='This program processes files from the Earth System Grid Federation (ESGF) archive.',
-		license='Public',
-		platforms='Linux',
-		author='jripsl',
-		author_email='jripsl@ipsl.jussieu.fr'
-		)
+        url='https://github.com/Prodiguer/synda',
+        version='1.0',
+        description='ESGF Data Processing Program',
+        long_description='This program processes files from the Earth System Grid Federation (ESGF) archive.',
+        license='Public',
+        platforms='Linux',
+        author='jripsl',
+        author_email='jripsl@ipsl.jussieu.fr'
+        )
