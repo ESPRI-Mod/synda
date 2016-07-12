db288d028d846f7f7fa997a473ba98b0437bfca3
jripsl <jripsl@outlook.com>
2016-07-12 13:22:00 +0200

    Add pipeline.

---
+++ sdp/conf/pipeline/IPSL_003.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+##################################
+#  @program        synchro-data
+#  @description    climate models data transfer program
+#  @copyright      Copyright “(c)2009 Centre National de la Recherche Scientifique CNRS. 
+#                             All Rights Reserved”
+#  @license        CeCILL (http://dods.ipsl.jussieu.fr/jripsl/synchro_data/LICENSE)
+##################################
+
+"""Contains 'dataset latest' pipeline definition."""
+
+import sppipelineutils
+from sppostprocessingutils import PostProcessingPipeline,State,Transition
+
+def get_pipeline():
+    return ppp
+
+def set_variable_path_type(kw):
+    assert kw.variable!='' # additional check just in case
+    kw.path_type='variable'
+
+# init.
+
+def f1(kw):
+    set_variable_path_type(kw)
+    path=sppipelineutils.build_process_path(kw)
+    return {'project':kw.project,'variable_path':path}
+def f2(kw):
+    set_variable_path_type(kw)
+    src_path=sppipelineutils.build_mirror_path(kw)
+    dest_path=sppipelineutils.build_process_path(kw)
+    return {'project':kw.project,'src_variable_path':src_path,'dest_variable_path':dest_path}
+def f3(kw):
+    set_variable_path_type(kw)
+    path=sppipelineutils.build_user_path('main',kw)
+    return {'project':kw.project,'variable_path':path}
+def f4(kw):
+    set_variable_path_type(kw)
+    src_path=sppipelineutils.build_process_path(kw)
+    dest_path=sppipelineutils.build_user_path('main',kw)
+    return {'project':kw.project,'src_variable_path':src_path,'dest_variable_path':dest_path}
+def f5(kw):
+    set_variable_path_type(kw)
+    path=sppipelineutils.build_user_path('main',kw)
+    return {'project':kw.project,'dataset_path':path}
+
+name='IPSL_003'
+ppp=PostProcessingPipeline(name)
+
+t1=Transition(name='suppression_variable',destination='S0200',get_args=f1)
+t2=Transition(name='coalesce',destination='S0300',get_args=f2)
+t3=Transition(name='overlap',destination='S0400',get_args=f1)
+t4=Transition(name='time_axis_normalization',destination='S0500',get_args=f1)
+t5=Transition(name='suppression_variable',destination='S0600',get_args=f3)
+t6=Transition(name='copy',destination='S0700',get_args=f4)
+t7=Transition(name='cdscan',destination='S0800',get_args=f4)
+t8=Transition(name='latest',destination='S0900',get_args=f5)
+t9=Transition(name='mapfile',destination='S1000',get_args=f5)
+
+s1=State(name='S0100',transition=t1,initial=True)
+s2=State(name='S0200',transition=t2)
+s3=State(name='S0300',transition=t3)
+s4=State(name='S0400',transition=t4)
+s5=State(name='S0500',transition=t5)
+s6=State(name='S0600',transition=t6)
+s7=State(name='S0700',transition=t7)
+s8=State(name='S0800',transition=t8)
+s9=State(name='S0900',transition=t9)
+s10=State(name='S1000',transition=None)
+
+ppp.add(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10)
+
