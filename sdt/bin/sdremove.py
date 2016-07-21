33e82702f15e092a789f861e6e2d453cce52492d
jripsl <jripsl@outlook.com>
2016-07-21 17:06:54 +0200

    Add non-interactive mode to 'synda remove' command.

--- sdt/bin/sdremove.py
+++ sdt/bin/sdremove.py
@@ -46,12 +46,21 @@ def run(args):
             print_stderr('%i file(s) will be removed.'%count_delete)
             print_stderr('After this operation, %s of disk space will be freed.'%humanize.naturalsize(size_delete,gnu=False))
 
-            if sdutils.query_yes_no('Do you want to continue?', default="no"):
+            # ask user for confirmation
+            if interactive:
+                if sdutils.query_yes_no('Do you want to continue?', default="no"):
+                    suppression_confirmed=True
+                else:
+                    print_stderr('Abort.')
+                    return 1
+            else:
+                suppression_confirmed=True
+
+            # perform deletion
+            if suppression_confirmed:
                 remove(files)
                 return 0
-            else:
-                print_stderr('Abort.')
-                return 1
+
         else:
             print_stderr('Nothing to delete.')
             return 0
