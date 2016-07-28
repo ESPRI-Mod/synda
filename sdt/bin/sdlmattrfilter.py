84ea0b0b3bdd3ac4facbfb624cc0f92ecdab5f41
jripsl <jripsl@outlook.com>
2016-07-28 14:48:37 +0200

    Add check.

--- sdt/bin/sdlmattrfilter.py
+++ sdt/bin/sdlmattrfilter.py
@@ -21,6 +21,8 @@ import sdpipelineprocessing
 
 def run(metadata,attrs_to_keep):
 
+    assert not isinstance(metadata.store,list)
+
     metadata_cpy=metadata.copy() # prevent modify original data
 
     assert not isinstance(metadata_cpy.store,list)
