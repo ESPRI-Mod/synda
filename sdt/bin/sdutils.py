bb1b9787ae8a48161833cf7948aca528b88348dd
jripsl <jripsl@outlook.com>
2016-09-01 03:17:35 +0200

    Write yes/no (interactive confirmation msg) on stderr.

--- sdt/bin/sdutils.py
+++ sdt/bin/sdutils.py
@@ -70,14 +70,14 @@ def query_yes_no(question, default="yes"):
         raise ValueError("invalid default answer: '%s'" % default)
 
     while True:
-        sys.stdout.write(question + prompt)
+        sys.stderr.write(question + prompt)
         choice = raw_input().lower()
         if default is not None and choice == '':
             return valid[default]
         elif choice in valid:
             return valid[choice]
         else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
+            sys.stderr.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
 
 def get_status(args, **kwargs):
