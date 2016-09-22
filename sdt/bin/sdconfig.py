8a2f27c93e59a315688984e7cabb12c83d2a231f
jripsl <jripsl@outlook.com>
2016-09-22 18:25:09 +0200

    type now depends on server_type.

--- sdt/bin/sdconfig.py
+++ sdt/bin/sdconfig.py
@@ -218,7 +218,8 @@ use_myproxy_module=True
 # Type of metadata server. Default is 'esgf_search_api'.
 metadata_server_type='esgf_search_api' # 'esgf_search_api' | 'thredds_catalog' | 'apache_default_listing'
 
-sdtsaction_type_default=sdconst.SA_TYPE_DATASET
+# Set default type (File | Dataset | Variable)
+sdtsaction_type_default=sdconst.SA_TYPE_FILE if metadata_server_type=='apache_default_listing' else sdconst.SA_TYPE_DATASET
 
 mono_host_retry=False
 proxymt_progress_stat=False
