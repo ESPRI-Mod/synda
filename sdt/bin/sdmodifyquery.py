bd234d7d24cab355359a3a67b7953d75ccd0f93a
jripsl <jripsl@outlook.com>
2016-11-06 16:21:33 +0100

    Improve wipeout func.

--- sdt/bin/sdmodifyquery.py
+++ sdt/bin/sdmodifyquery.py
@@ -43,9 +43,9 @@ def change_priority(new_priority,conn=sddb.conn):
     conn.commit()
     c.close()
 
-def wipeout_datasets_flags(conn=sddb.conn):
+def wipeout_datasets_flags(status=None,latest=0,conn=sddb.conn):
     """Reset flags on all datasets."""
     c=conn.cursor()
-    c.execute("update dataset set status=?, latest=?",(None,0,))
+    c.execute("update dataset set status=?, latest=?",(status,latest,))
     conn.commit()
     c.close()
