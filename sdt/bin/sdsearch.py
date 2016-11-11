913a233ece846ba9f0e5f5b6be6dd0bd90b1cac2
jripsl <jripsl@outlook.com>
2016-11-12 00:17:34 +0100

    Improve logging.

--- sdt/bin/sdsearch.py
+++ sdt/bin/sdsearch.py
@@ -93,9 +93,9 @@ def execute_queries(squeries,parallel,post_pipeline_mode,action):
     metadata=sdpipeline.post_pipeline(metadata,post_pipeline_mode)
     sdlog.info("SDSEARCH-594","Metadata processing end")
 
-    sdlog.info("SDSEARCH-620","Retrieve timestamps")
+    sdlog.info("SDSEARCH-620","Retrieve timestamps begins")
     metadata=fill_dataset_timestamp(squeries,metadata,parallel,action) # complete missing info
-    sdlog.info("SDSEARCH-634","timestamps successfully retrieved")
+    sdlog.info("SDSEARCH-634","Retrieve timestamps ends")
 
     return metadata
 
