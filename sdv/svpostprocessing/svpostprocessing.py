52dedaac4fdc003043aba2670f2fb162c41c3e8f
jripsl <jripsl@outlook.com>
2016-11-15 14:07:16 +0100

    UAT impl.

--- sdv/svpostprocessing/svpostprocessing.py
+++ sdv/svpostprocessing/svpostprocessing.py
@@ -27,41 +27,63 @@ import testlib.svtestcommon as tc
 def run():
     task_exec(confirm_test_platform_is_ready)
 
-    task_exec(tc.stop)
     task_exec(tc.execute_basic_command)
     task_exec(tc.check_version)
 
     task_exec(reset_test_platform)
 
-    task_exec(install_default_files)
-    task_exec(install_fake_scripts)
+    task_exec(tc.stop_sdt)
+    task_exec(tc.stop_sdp)
 
-    #task_exec(check_dataset_version)
-    #time.sleep(150) # give some time for the file to be downloaded (default 300)
-    #task_exec(check_dataset_version)
+    # discovery
+    task_exec(install_CMIP5)
+    task_exec(check_CMIP5_installation_result)
+    task_exec(install_CORDEX)
+    task_exec(check_CORDEX_installation_result)
+
+    # download
+    task_exec(tc.start_sdt)
+    time.sleep(300) # give some time for the file to be downloaded
+    task_exec(check_download_result)
+
+    # pexec
+    task_exec(trigger_CDF)
+    task_exec(check_CDF_trigger_result)
+
+    # pp events transfer
+    task_exec(tc.start_sdp)
+    time.sleep(50) # give some time for pp events to be transfered from SDT to SDP
+    task_exec(check_pp_events_transfer_result)
+
+    # start pp pipelines
+    task_exec()
+
+@task
+def confirm_test_platform_is_ready():
+    fabric_run('sudo synda install -y cmip5.output1.MPI-M.MPI-ESM-LR.decadal1995.mon.land.Lmon.r2i1p1.v20120529 baresoilFrac')
 
 @task
 def reset_test_platform():
     fabric_run('%s'%reset_script)
 
 @task
-def check_dataset_version():
-    fabric_run('sudo synda install -y cmip5.output1.MPI-M.MPI-ESM-LR.decadal1995.mon.land.Lmon.r2i1p1.v20120529 baresoilFrac')
-
-@task
-def check_dataset_version():
+def install_CMIP5():
     fabric_run('test -f /tmp/foobar')
 
 @task
-def check_dataset_version():
+def install_CORDEX():
     fabric_run('sudo synda stat CMIP5 MPI-M MPI-ESM-LR decadal1995 mon baresoilFrac | grep "Done files count: 1"')
 
 @task
-def check_dataset_version():
+def trigger_CDF():
     fabric_run('sudo synda remove -y CMIP5 MPI-M MPI-ESM-LR decadal1995 mon baresoilFrac')
 
 @task
-def check_dataset_version():
+def start_pp_pipelines():
+    fabric_run('synda_wo -x start')
+
+@task
+def fake():
     fabric_run('test ! -f /srv/synda/sdt/data/cmip5/output1/MPI-M/MPI-ESM-LR/decadal1995/mon/land/Lmon/r2i1p1/v20120529/baresoilFrac/baresoilFrac_Lmon_MPI-ESM-LR_decadal1995_r2i1p1_199601-200512.nc')
 
 # init.
