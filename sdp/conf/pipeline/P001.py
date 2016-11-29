ff2be9be7dbc1c143947d356d16a7eac60ef35c9
jripsl <jripsl@outlook.com>
2016-11-29 12:03:00 +0100

    Improve sample.

--- sdp/conf/pipeline/P001.py
+++ sdp/conf/pipeline/P001.py
@@ -17,45 +17,18 @@ from sppostprocessingutils import PostProcessingPipeline,State,Transition
 def get_pipeline():
     return ppp
 
-def set_variable_path_type(kw):
-    assert kw.variable!='' # additional check just in case
-    kw.path_type='variable'
-
 # init.
-
-def f1(kw):
-    set_variable_path_type(kw)
-    path=sppipelineutils.build_process_path(kw)
-    return {'project':kw.project,'variable_path':path}
-def f2(kw):
-    set_variable_path_type(kw)
-    src_path=sppipelineutils.build_mirror_path(kw)
-    dest_path=sppipelineutils.build_process_path(kw)
-    return {'project':kw.project,'src_variable_path':src_path,'dest_variable_path':dest_path}
-def f3(kw):
-    set_variable_path_type(kw)
-    path=sppipelineutils.build_user_path('main',kw)
-    return {'project':kw.project,'variable_path':path}
-def f4(kw):
-    set_variable_path_type(kw)
-    src_path=sppipelineutils.build_process_path(kw)
-    dest_path=sppipelineutils.build_user_path('main',kw)
-    return {'project':kw.project,'src_variable_path':src_path,'dest_variable_path':dest_path}
-def f5(kw):
-    set_variable_path_type(kw)
-    path=sppipelineutils.build_user_path('main',kw)
-    return {'project':kw.project,'dataset_path':path}
 
 name='P001'
 ppp=PostProcessingPipeline(name)
 
-t0=Transition(name='is_duplicate',destination={0:'S0300',1:'S0100'})
-t1=Transition(name='time_axis_normalization',destination='S0200',get_args=f1)
-t2=Transition(name='latest',destination='S0300',get_args=f5)
+t0=Transition(name='remove_duplicate',destination='S0200')
+t1=Transition(name='time_axis_normalization',destination='S0300')
+t2=Transition(name='remove_tmp_files',destination='S0400')
 
-s0=State(name='S0090',transition=t0,initial=True)
-s1=State(name='S0100',transition=t1)
-s2=State(name='S0200',transition=t2)
-s3=State(name='S0300',transition=None)
+s0=State(name='S0100',transition=t0,initial=True)
+s1=State(name='S0200',transition=t1)
+s2=State(name='S0300',transition=t2)
+s3=State(name='S0400',transition=None)
 
 ppp.add(s0,s1,s2,s3)
