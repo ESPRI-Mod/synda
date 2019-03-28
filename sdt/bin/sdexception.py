c11d256b532623e70bb442184a43131eabc24b01
Atef <abennasser@ipsl.jussieu.fr>
2019-03-28 11:08:11 +0100

    Revert "More fallbacks after a url fails, including different data nodes"

--- sdt/bin/sdexception.py
+++ sdt/bin/sdexception.py
@@ -1,12 +1,11 @@
-#!/usr/share/python/synda/sdt/bin/python
-#jfp was
+#!/usr/bin/env python
 # -*- coding: ISO-8859-1 -*-
 
 ##################################
 #  @program        synda
 #  @description    climate models data transfer program
-#  @copyright      Copyright "(c)2009 Centre National de la Recherche Scientifique CNRS. 
-#                             All Rights Reserved"
+#  @copyright      Copyright “(c)2009 Centre National de la Recherche Scientifique CNRS. 
+#                             All Rights Reserved”
 #  @license        CeCILL (https://raw.githubusercontent.com/Prodiguer/synda/master/sdt/doc/LICENSE)
 ##################################
 
@@ -27,8 +26,6 @@ class SDException(Exception):
 class FileNotFoundException(SDException):
     pass
 class HttpUrlNotFoundException(SDException):
-    pass
-class NextUrlNotFoundException(SDException):
     pass
 class NoTransferWaitingException(SDException):
     pass
