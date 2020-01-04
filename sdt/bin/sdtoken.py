a4cd23b13292d057ee85916a8d4979cb3260ec6c
Lukasz Lacinski <lukasz@uchicago.edu>
2020-01-04 14:24:19 -0600

    Support Python 2 and 3

--- sdt/bin/sdtoken.py
+++ sdt/bin/sdtoken.py
@@ -26,6 +26,12 @@ scopes = ('openid email profile '
           'urn:globus:auth:scope:transfer.api.globus.org:all')
 
 
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
 def load_tokens_from_file(filepath):
     """Load a set of saved tokens."""
     with open(filepath, 'r') as f:
@@ -58,7 +64,7 @@ def do_native_app_authentication(client_id, redirect_uri,
     print("Native App Authorization URL: \n{}".format(url))
     if not is_remote_session():
         webbrowser.open(url, new=1)
-    auth_code = raw_input("Enter the auth code: ").strip()
+    auth_code = input("Enter the auth code: ").strip()
     token_response = client.oauth2_exchange_code_for_tokens(auth_code)
     # return a set of tokens, organized by resource server name
     return token_response.by_resource_server
