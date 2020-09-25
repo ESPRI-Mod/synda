fff3c8100df1f41df99a8e237151b47bc66688e7
AtefBN <Homersimpson_666>
2020-09-25 17:48:05 +0200

    entry point moved to setup.py instead of meta.yaml file

--- sdt/setup.py
+++ sdt/setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
 import os
 
+from setuptools import setup
 setup(
     name='synda',
     scripts=[os.path.join('bin', f) for f in os.listdir('bin')],
@@ -8,8 +8,13 @@ setup(
     package_data={
         'data': ['data_package/data.tar.gz'],
     },
+    entry_points={
+        "console_scripts": [
+            "synda=synda:run"
+        ]
+    },
     url='https://github.com/Prodiguer/synda',
-    version='3.11',
+    version='3.12',
     description='ESGF Data transfer Program',
     long_description='This program download files from the Earth System Grid Federation (ESGF) '
                      'archive using command line.',
