543d9297b799f894834aa58af83bfaae3dc0affc
Jeffrey Farrar Painter <painter1@llnl.gov>
2020-12-09 11:52:35 -0800

    Prioritize the next url by data node before protocol, and don't fall back from another data node to LASG, one of the slow nodes.

--- sdt/bin/sdnexturl.py
+++ sdt/bin/sdnexturl.py
@@ -67,7 +67,7 @@ def run(tr):
         conn.close()
 
 def next_url(tr,conn):
-    all_urlps=get_urls(tr.file_functional_id,tr.searchapi_host) # [[url1,protocol1],[url2,protocol2],...]
+    all_urlps=get_urls(tr.file_functional_id,tr.searchapi_host,tr.url) # [[url1,protocol1],[url2,protocol2],...]
     sdlog.info("SDNEXTUR-006","all_urpls= %s"%(all_urlps,))
     c = conn.cursor()
     fus = c.execute("SELECT url FROM failed_url WHERE file_id="+
@@ -95,7 +95,7 @@ def remove_unsupported_url(urlps):
     # remove opendap and globus urls (opendap and globus are not used in synda for now)
     return [ urlp[0] for urlp in urlps if (urlp[1].find('opendap')<0 and urlp[1].find('globus')<0)]
 
-def get_urls(file_functional_id, searchapi_host):
+def get_urls(file_functional_id, searchapi_host, old_url):
     """returns a prioritized list of [url,protocol] where each url can supply the specified file"""
 
     try:
@@ -134,14 +134,19 @@ def get_urls(file_functional_id, searchapi_host):
         # The search for //None bypasses an issue with the SOLR lookup where there is no
         # url_gridftp possibility.
 
-    return prioritize_urlps( urlps )
+    return prioritize_urlps( urlps, old_url )
 
 url_fields=','.join(sdconst.URL_FIELDS)  # used for the sdquicksearch call above
 
-def prioritize_urlps( urlps ):
+def prioritize_urlps( urlps, old_url ):
     """Orders a list urlps so that the highest-priority urls come first.  urlps is a list of
-    lists of the form [url,protocol].  First, GridFTP urls are preferred over everything else.
-    Then, prefer some data nodes over others."""
+    lists of the form [url,protocol].
+    Some data nodes are preferred over others.  Then, GridFTP is preferred over http."""
+    # Formerly, I prioritized the other way; but experience shows that many data nodes which
+    # officially support GridFTP, don't usually have it working.
+    # Note also that within this function a "high priority" url has a low priority number.
+    # That's just for programming convenience.
+
     def priprotocol(protocol):
         if protocol.find('gridftp')>0:  return 0
         if protocol.find('http')>0:     return 1
@@ -153,8 +158,11 @@ def prioritize_urlps( urlps ):
         if url.find('ceda')>0:  return 3
         if url.find('dkrz')>0:  return 4
         if url.find('nci')>0:   return 5
+        if old_url.find('lasg')<0 and url.find('lasg')>0:
+            return 99  # Never fall back to this very slow data node; but changing protocol is ok.
         return 6
-    return sorted( urlps, key=(lambda urlp: (priprotocol(urlp[1]), priurl(urlp[0]))) )
+    urlps_cleaned = [ url for url in urlps if priurl(url)<99 ]
+    return sorted( urlps_cleaned, key=(lambda urlp: ( priurl(urlp[0]), priprotocol(urlp[1]))) )
 
 
 if __name__ == '__main__':
